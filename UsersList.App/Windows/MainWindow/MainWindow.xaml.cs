0c207f6ffe1c528339afb07dac0e04e28df7ecdb
Starinin Andrey <starininandrey@gmail.com>
2022-09-12 16:51:45 +0300

    Update MainWindow

--- UsersList.App/Windows/MainWindow/MainWindow.xaml.cs
+++ UsersList.App/Windows/MainWindow/MainWindow.xaml.cs
@@ -1,12 +1,27 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using UsersList.App.Components.UserCardComponent;
+using UsersList.App.Models;
 
 namespace UsersList.App.Windows.MainWindow
 {
     public partial class MainWindow : Window
     {
+        private IEnumerable<User> _users;
+
         public MainWindow()
         {
+            _users = Users.GetUsers();
             InitializeComponent();
+            InitUserPanel();
+        }
+
+        private void InitUserPanel()
+        {
+            foreach (var user in _users)
+            {
+                UsersPanel.Children.Add(new UserCardComponent(user));
+            }
         }
     }
 }
