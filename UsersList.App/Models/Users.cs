18cb3509dfc5aab43cfe6984de1c84a18a4881fd
Starinin Andrey <starininandrey@gmail.com>
2022-09-12 16:51:24 +0300

    Add Users

---
+++ UsersList.App/Models/Users.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace UsersList.App.Models;
+
+public static class Users
+{
+    public static IEnumerable<User> GetUsers()
+    {
+        return new List<User>();
+    }
+}
